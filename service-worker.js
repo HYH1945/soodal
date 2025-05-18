7f0dd19d9bcc52dfe89ef5d3c11929506f8bbb23
belowzero <dudgkdlqslek@gmail.com>
2025-05-19 05:55:03 +0900

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'test';
+var cacheName = '수달';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
